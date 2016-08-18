750d8206e6b7e3a8f31e1367380e16530b47d6bb
Soham Gupta <soham.gupta@geotechinfo.net>
2016-08-18 22:33:01 +0530

    Changes

--- ResultCaptureApp/client/templates/loginTemplate/loginDefault.js
+++ ResultCaptureApp/client/templates/loginTemplate/loginDefault.js
@@ -1,72 +1,72 @@
 
 Template.loginDefault.events({
     'click #facebook-login': function (event) {
-        Meteor.loginWithFacebook({ requestPermissions: ['user_friends', 'public_profile', 'email'] }, (err) => {
-            if (err) {
-                alert("Facebook login failed");
-            } else {
-                				if(Meteor.user().emails[0].verified !== true){
-					Router.go('/verifyEmail/' + Meteor.user().emails[0]);
-				}else{
-					Router.go('/dashboard');
-				}
-            }
-        });
+        // Meteor.loginWithFacebook({ requestPermissions: ['user_friends', 'public_profile', 'email'] }, (err) => {
+        //     if (err) {
+        //         alert("Facebook login failed");
+        //     } else {
+        //         				if(Meteor.user().emails[0].verified !== true){
+		// 			Router.go('/verifyEmail/' + Meteor.user().emails[0]);
+		// 		}else{
+		// 			Router.go('/dashboard');
+		// 		}
+        //     }
+        // });
     },
     'click #google-login': function () {
-        Meteor.loginWithGoogle({}, (err) => {
-			debugger;
-            if (err) {
-                alert("Google login failed");
-            } else {
-				if(Meteor.user().emails[0].verified !== true){
-					Router.go('/verifyEmail/' + Meteor.user().emails[0]);
-				}else{
-					Router.go('/dashboard');
-				}
+        // Meteor.loginWithGoogle({}, (err) => {
+		// 	debugger;
+        //     if (err) {
+        //         alert("Google login failed");
+        //     } else {
+		// 		if(Meteor.user().emails[0].verified !== true){
+		// 			Router.go('/verifyEmail/' + Meteor.user().emails[0]);
+		// 		}else{
+		// 			Router.go('/dashboard');
+		// 		}
                 
-            }
-        });
+        //     }
+        // });
     },
     'click #linkedIn-login': function () {
-        Meteor.loginWithLinkedin({}, (err) => {
-            if (err) {
-                alert("LinkedIn login failed");
-            } else {
-				var u = Meteor.user();
-				if(u !== undefined){
-					if(u.emails[0].verified !== true){
-						Meteor.users.update({ _id: u._id }, {
-							$set:
-							{
-								'emails.0.address':u.services.linkedin.emailAddress
-							}
-						});
-						Router.go('/verifyEmail/' + u.services.linkedin.emailAddress);
-					} else {
-						Router.go('/dashboard');
-					}
-				}
-            }
-        });
+        // Meteor.loginWithLinkedin({}, (err) => {
+        //     if (err) {
+        //         alert("LinkedIn login failed");
+        //     } else {
+		// 		var u = Meteor.user();
+		// 		if(u !== undefined){
+		// 			if(u.emails[0].verified !== true){
+		// 				Meteor.users.update({ _id: u._id }, {
+		// 					$set:
+		// 					{
+		// 						'emails.0.address':u.services.linkedin.emailAddress
+		// 					}
+		// 				});
+		// 				Router.go('/verifyEmail/' + u.services.linkedin.emailAddress);
+		// 			} else {
+		// 				Router.go('/dashboard');
+		// 			}
+		// 		}
+        //     }
+        // });
     },
     'click #logout': function (event) {
-        Meteor.logout(function (err) {
-            if (err) {
-                throw new Meteor.Error("Logout failed");
-            }
-        })
+        // Meteor.logout(function (err) {
+        //     if (err) {
+        //         throw new Meteor.Error("Logout failed");
+        //     }
+        // })
     },
 
     'click #forgotPasswordBtn': (event) => {
-        Router.go('/forgotPassword');
+        //Router.go('/forgotPassword');
     },
 
     'click #signInBtn': (event) => {
-        Router.go('/loginEmail');
+       // Router.go('/loginEmail');
     },
 
     'click #signUpBtn': (event) => {
-        Router.go('/signupemail');
+       // Router.go('/signupemail');
     }
 });
